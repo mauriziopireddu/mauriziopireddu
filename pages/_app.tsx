91b128b24e8879bfd650cf4ea2773b76ff7dd794
Maurizio Pireddu <pireddu.maurizio@gmail.com>
2021-01-24 11:07:50 +0000

    add sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,19 @@ import "styles/tailwind.css";
 import "styles/cobalt2.prism.css";
 import { Breadcrumbs } from "components/Breadcrumbs";
 import { Title } from "components/Title";
+import { useSentry } from "lib/useSentry";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Title />
-    <div className="container mx-auto max-w-screen-lg px-4">
-      <Breadcrumbs />
-      <Component {...pageProps} />
-    </div>
-  </>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useSentry();
+  return (
+    <>
+      <Title />
+      <div className="container mx-auto max-w-screen-lg px-4">
+        <Breadcrumbs />
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+};
 
 export default MyApp;
